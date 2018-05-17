66f633df817f4fdb3dda1398ff0f310c2d5e2762
Stefan Rimaila <stefan@rimaila.fi>
2018-05-18 01:48:40 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+NAME = 'pyles'
+VERSION = '0.1'
+DESCRIPTION = 'Now you will have pyles of data!'
+AUTHOR = 'Stefan Rimaila'
+AUTHOR_EMAIL = 'stefan@rimaila.fi'
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL)
